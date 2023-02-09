b5b1c3938bd76ba794fccc0857955e0801a7129d
federg <federicorg07@gmail.com>
2023-02-09 15:35:29 -0300

    subiendo estructura

---
+++ react-advanced/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+
+function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [characters, setCharacters] = useState([]);
+
+  const getCharacter = async() => {
+    setLoading(true);
+    const {data} = await axios('https://rickandmortyapi.com/api/character');
+    setCharacters(data.results);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getCharacter();
+  }, [])
+  console.log(characters);
+  
+  
+  return (
+    <div className="App">
+      {
+        loading && <p>Cargando....</p>
+      }
+      {
+        characters.map(character => 
+          <h1 key={character.id}>{character.name}</h1>)
+      }
+    </div>
+  );
+}
+
+export default App;
